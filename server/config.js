f0c82ef647af3d36b06477e53116c8dbbb607284
Antonio Gabriel Fernandes <antgabrielsf@gmail.com>
2023-03-06 13:30:54 -0300

    Initial commit

---
+++ server/config.js
@@ -0,0 +1,15 @@
+const MYSQL_HOST = "localhost";
+const MYSQL_USER = "root";
+const MYSQL_PASSWORD = "password";
+const MYSQL_DATABASE = "gameshop";
+const MYSQL_TABLE = "games";
+const LISTEN_PORT = 3001;
+
+module.exports = {
+    MYSQL_HOST,
+    MYSQL_USER,
+    MYSQL_PASSWORD,
+    MYSQL_DATABASE,
+    MYSQL_TABLE,
+    LISTEN_PORT,
+};
---
+++ server/index.js
@@ -0,0 +1,81 @@
+const config = require("./config")
+const express = require("express");
+const app = express();
+const mysql = require("mysql2");
+const cors = require("cors");
+
+const db = mysql.createPool({
+  host: config.MYSQL_HOST,
+  user: config.MYSQL_USER,
+  password: config.MYSQL_PASSWORD,
+  database: config.MYSQL_DATABASE,
+});
+
+app.use(express.json());
+app.use(cors());
+
+app.post("/register", (req, res) => {
+  const { name } = req.body;
+  const { cost } = req.body;
+  const { category } = req.body;
+
+  let mysql = `INSERT INTO ${config.MYSQL_TABLE} ( name, cost, category) VALUES (?, ?, ?)`;
+  db.query(mysql, [name, cost, category], (err, result) => {
+    res.send(result);
+  });
+});
+
+app.post("/search", (req, res) => {
+  const { name } = req.body;
+  const { cost } = req.body;
+  const { category } = req.body;
+
+  let mysql =
+    `SELECT * from ${config.MYSQL_TABLE} WHERE name = ? AND cost = ? AND category = ?`;
+  db.query(mysql, [name, cost, category], (err, result) => {
+    if (err) res.send(err);
+    res.send(result);
+  });
+});
+
+app.get("/getCards", (req, res) => {
+  let mysql = `SELECT * FROM ${config.MYSQL_TABLE}`;
+  db.query(mysql, (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
+
+app.put("/edit", (req, res) => {
+  const { id } = req.body;
+  const { name } = req.body;
+  const { cost } = req.body;
+  const { category } = req.body;
+  let mysql = `UPDATE ${config.MYSQL_TABLE} SET name = ?, cost = ?, category = ? WHERE id = ?`;
+  db.query(mysql, [name, cost, category, id], (err, result) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
+
+app.delete("/delete/:id", (req, res) => {
+  const { id } = req.params;
+  let mysql = `DELETE FROM ${config.MYSQL_TABLE} WHERE id = ?`;
+  db.query(mysql, id, (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
+
+app.listen(config.LISTEN_PORT, () => {
+  console.log(`rodando na porta ${config.LISTEN_PORT}`);
+});
